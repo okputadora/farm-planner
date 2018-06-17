81a5fc5028ceb4b8876cc83d974db2a42649c553
okputadora <okputadora@gmail.com>
2018-06-17 15:57:22 -0400

    added login to state

--- client/src/Components/Form/Input/Input.js
+++ client/src/Components/Form/Input/Input.js
@@ -2,7 +2,13 @@ import React from 'react';
 import classes from './input.css';
 const input = props => {
   return (
-    <input type={props.type} className={classes.Input} placeholder={props.placeholder}/>
+    <input
+      type={props.type}
+      className={classes.Input}
+      placeholder={props.placeholder}
+      onChange={props.change}
+      name={props.name}
+    />
   )
 }
 
--- client/src/Containers/Layout/LoginSignup/LoginSignup.js
+++ client/src/Containers/Layout/LoginSignup/LoginSignup.js
@@ -5,6 +5,24 @@ import Input from '../../../Components/Form/Input/Input';
 import Button from '../../../Components/Button/MainButton/MainButton';
 import classes from './loginSignup.css';
 class LoginSignUp extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    password: '',
+  }
+
+  onChangeHandler = (event) => {
+    console.log(event.target.name);
+    console.log(event.target.value);
+    const {name, value} = event.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
+
   render(){
     return (
       <Aux>
@@ -12,10 +30,11 @@ class LoginSignUp extends Component {
         <div className={classes.Login}>
           <h3>Login/Signup</h3>
           <div className={classes.LoginCard}>
-            <Input placeholder="First Name" type="text"/>
-            <Input placeholder="Last Name" type="text"/>
-            <Input placeholder="email" type="email"/>
-            <Input placeholder="password" type="password"/>
+            <Input change={this.onChangeHandler} name="firstName" placeholder="First Name" type="text"/>
+            <Input change={this.onChangeHandler} name="lastName" placeholder="Last Name" type="text"/>
+            <Input change={this.onChangeHandler} name="username" placeholder="username" type="text"/>
+            <Input change={this.onChangeHandler} name="email" placeholder="email" type="email"/>
+            <Input change={this.onChangeHandler} name="password" placeholder="password" type="password"/>
             <Button>Start Your Season</Button>
           </div>
         </div>

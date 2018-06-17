f2e4b1de9a18d95077709f0ef9890c5a6fff7ab5
okputadora <okputadora@gmail.com>
2018-06-17 13:29:36 -0400

    updated user model

--- models/User.js
+++ models/User.js
@@ -3,6 +3,10 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const User = new mongoose.Schema({
   username: {type: String, required: true},
   password: {type: String, required: true},
+  location: {
+    town: {type: String},
+    coords: {lat: {type: Number}, lng: {type: Number}}
+  }
 })
 
 module.exports = mongoose.model('User', User);
